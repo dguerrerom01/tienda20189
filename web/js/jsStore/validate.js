f182538213bc6921928b1b61ce1edc6e36acdec7
luciano <luciano@bme.es>
2018-10-23 12:54:06 +0200

    ver con dni

--- web/js/jsStore/validate.js
+++ web/js/jsStore/validate.js
@@ -82,7 +82,6 @@ STORE.namespace('STORE.ValidacionUtil');
     }
 })(window)
 
-
 STORE.namespace('STORE.ValidacionExpresionRegular');
 (function(g){
 
@@ -128,6 +127,56 @@ STORE.namespace('STORE.ValidacionExpresionRegular');
             parametro.mensajeError = "ERROR: Código Posrtal 5 caracteres";
 
             STORE.ValidacionUtil.valorarConsecuencia(STORE.ValidacionUtil.validarExpRegular(parametro),parametro);
+        },
+
+        validarEmail : function (evt) {
+
+            parametro.nodo = evt.target;
+
+            parametro.patron = "^([a-zA-Z0-9.!#$%&'+/=?^_`{|}~-]+@[a-zA-Z0-9-]{3,20}(?:\\.[a-zA-Z0-9-]{2,3}))$";
+
+            parametro.mensajeError = "Email no valido";
+
+            STORE.ValidacionUtil.valorarConsecuencia(STORE.ValidacionUtil.validarExpRegular(parametro), parametro);
+        },
+
+        validarDni : function (evt) {
+
+            parametro.nodo = evt.target;
+
+            parametro.patron = "^[0-9]{8}[TRWAGMYFPDXBNJZSQVHLCKE]$";
+
+            if(!STORE.ValidacionExpresionRegular(parametro.patron)){
+
+                parametro.mensajeError = "ERROR: Dni incorrecto";
+
+                STORE.ValidacionUtil.valorarConsecuencia(false, parametro);
+
+            }
+
+            else{
+                //TODO comprobar la letra
+                let letra = true; //TODO false parametro.mensajeError = "ERROR: Letra Dni incorrecto";
+
+                STORE.ValidacionUtil.valorarConsecuencia(letra, parametro);
+
+            }
+
+
+!C
+
+        },
+
+        validarPassword : function (evt) {
+            parametro.nodo = evt.target;
+
+            parametro.patron =  "^([A-Za-z0-9]){8,}$";
+
+            parametro.mensajeError = "ERROR: Contraseña incorrecta ";
+
+            parametro.text = parametro.nodo.value;
+
+            STORE.ValidacionUtil.valorarConsecuencia(STORE.ValidacionUtil.validarExpRegular(parametro), parametro);
         }
     }
 
